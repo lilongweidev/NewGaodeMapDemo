56b60e74134b76a18ff35fcb41d8448e20419d11
longwei.li <lonelyholiday@qq.com>
2024-06-03 08:39:27 +0800

    提交

---
+++ app/src/main/java/com/llw/newmapdemo/MainActivity.java
@@ -0,0 +1,62 @@
+package com.llw.newmapdemo;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.llw.newmapdemo.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private ActivityMainBinding binding;
+    // 请求权限意图
+    private ActivityResultLauncher<String> requestPermission;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestPermission = registerForActivityResult(new ActivityResultContracts.RequestPermission(), result -> {
+            // 权限申请结果
+            Log.d(TAG, "权限申请结果: " + result);
+            showMsg(result ? "已获取到权限" : "权限申请失败");
+        });
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 检查是否已经获取到定位权限
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // 获取到权限
+            Log.d(TAG, "onResume: 已获取到权限");
+            showMsg("已获取到权限");
+        } else {
+            // 请求定位权限
+            requestPermission.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+    }
+
+    private void showMsg(CharSequence llw) {
+        Toast.makeText(this, llw, Toast.LENGTH_SHORT).show();
+    }
+}
