69c817a8ee5a8f93b888c7df42645e44817ecea6
longwei.li <lonelyholiday@qq.com>
2024-06-03 18:39:40 +0800

    完成定位信息获取。

---
+++ app/src/main/java/com/llw/newmapdemo/App.java
@@ -0,0 +1,26 @@
+package com.llw.newmapdemo;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.maps.MapsInitializer;
+import com.amap.api.services.core.ServiceSettings;
+
+public class App  extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Context mContext = this;
+        // 定位隐私政策同意
+        AMapLocationClient.updatePrivacyShow(mContext,true,true);
+        AMapLocationClient.updatePrivacyAgree(mContext,true);
+        // 地图隐私政策同意，因为使用的是轻量版地图合包，因此没有这个API，所以注释掉
+//        MapsInitializer.updatePrivacyShow(mContext,true,true);
+//        MapsInitializer.updatePrivacyAgree(mContext,true);
+        // 搜索隐私政策同意
+        ServiceSettings.updatePrivacyShow(mContext,true,true);
+        ServiceSettings.updatePrivacyAgree(mContext,true);
+    }
+}
--- app/src/main/java/com/llw/newmapdemo/MainActivity.java
+++ app/src/main/java/com/llw/newmapdemo/MainActivity.java
@@ -15,20 +15,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.llw.newmapdemo.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements AMapLocationListener {
     private static final String TAG = "MainActivity";
     private ActivityMainBinding binding;
     // 请求权限意图
     private ActivityResultLauncher<String> requestPermission;
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    //声明AMapLocationClientOption对象
+    public AMapLocationClientOption mLocationOption = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         requestPermission = registerForActivityResult(new ActivityResultContracts.RequestPermission(), result -> {
             // 权限申请结果
-            Log.d(TAG, "权限申请结果: " + result);
             showMsg(result ? "已获取到权限" : "权限申请失败");
         });
         super.onCreate(savedInstanceState);
@@ -40,6 +46,48 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // 初始化定位
+        initLocation();
+    }
+
+    /**
+     * 初始化定位
+     */
+    private void initLocation() {
+        try {
+            //初始化定位
+            mLocationClient = new AMapLocationClient(getApplicationContext());
+            //设置定位回调监听
+            mLocationClient.setLocationListener(this);
+            //初始化AMapLocationClientOption对象
+            mLocationOption = new AMapLocationClientOption();
+            //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            //获取最近3s内精度最高的一次定位结果
+            mLocationOption.setOnceLocationLatest(true);
+            //设置是否返回地址信息（默认返回地址信息）
+            mLocationOption.setNeedAddress(true);
+            //设置定位超时时间，单位是毫秒
+            mLocationOption.setHttpTimeOut(6000);
+            //给定位客户端对象设置定位参数
+            mLocationClient.setLocationOption(mLocationOption);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 开始定位
+     */
+    private void startLocation() {
+        if (mLocationClient != null) mLocationClient.startLocation();
+    }
+
+    /**
+     * 停止定位
+     */
+    private void stopLocation() {
+        if (mLocationClient != null) mLocationClient.stopLocation();
     }
 
     @Override
@@ -48,8 +96,8 @@ public class MainActivity extends AppCompatActivity {
         // 检查是否已经获取到定位权限
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             // 获取到权限
-            Log.d(TAG, "onResume: 已获取到权限");
-            showMsg("已获取到权限");
+            showMsg("开始定位");
+            startLocation();
         } else {
             // 请求定位权限
             requestPermission.launch(Manifest.permission.ACCESS_FINE_LOCATION);
@@ -58,5 +106,48 @@ public class MainActivity extends AppCompatActivity {
 
     private void showMsg(CharSequence llw) {
         Toast.makeText(this, llw, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 定位回调结果
+     */
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if (aMapLocation == null) {
+            showMsg("定位失败，aMapLocation 为空");
+            return;
+        }
+        // 获取定位结果
+        if (aMapLocation.getErrorCode() == 0) {
+            // 定位成功
+            showMsg("定位成功");
+//            aMapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+//            aMapLocation.getLatitude();//获取纬度
+//            aMapLocation.getLongitude();//获取经度
+//            aMapLocation.getAccuracy();//获取精度信息
+//            aMapLocation.getAddress();//详细地址，如果option中设置isNeedAddress为false，则没有此结果，网络定位结果中会有地址信息，GPS定位不返回地址信息。
+//            aMapLocation.getCountry();//国家信息
+//            aMapLocation.getProvince();//省信息
+//            aMapLocation.getCity();//城市信息
+            String result = aMapLocation.getDistrict();//城区信息
+//            aMapLocation.getStreet();//街道信息
+//            aMapLocation.getStreetNum();//街道门牌号信息
+//            aMapLocation.getCityCode();//城市编码
+//            aMapLocation.getAdCode();//地区编码
+//            aMapLocation.getAoiName();//获取当前定位点的AOI信息
+//            aMapLocation.getBuildingId();//获取当前室内定位的建筑物Id
+//            aMapLocation.getFloor();//获取当前室内定位的楼层
+//            aMapLocation.getGpsAccuracyStatus();//获取GPS的当前状态
+
+            binding.tvAddress.setText(result);
+            // 停止定位
+            stopLocation();
+        } else {
+            // 定位失败
+            showMsg("定位失败，错误：" + aMapLocation.getErrorInfo());
+            Log.e(TAG,"location Error, ErrCode:"
+                    + aMapLocation.getErrorCode() + ", errInfo:"
+                    + aMapLocation.getErrorInfo());
+        }
     }
 }
